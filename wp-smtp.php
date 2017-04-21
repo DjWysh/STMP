8f34999388196c27cc625ea24a046a9ed345ba47
DjWysh <djwysh@gmail.com>
2017-04-21 14:07:19 -0400

    Update wp-smtp.php

--- wp-smtp.php
+++ wp-smtp.php
@@ -3,19 +3,19 @@
 /*
 * SMTP
 *
-* @package   DjWysh SMTP
-* @author    Michael Mizner<mike@miznerism.com>
+* @package   DjWysh SMTP - Postmark
+* @author    Dj Wysh<djwysh@gmail.com>
 * @license   GPL-2.0+
-* @link      http://miznerism.com
-* @copyright 2015 Michael Mizner
+* @link      http://djwysh.rocks
+* @copyright 2017 Dj Wysh
 *
 * @wordpress-plugin
-* Plugin Name:       DjWysh SMTP
-* Plugin URI:        http://miznerism.com
+* Plugin Name:       DjWysh SMTP - Postmark
+* Plugin URI:        http://djwysh.rocks
 * Description:       Simple PHPMailer Plugin for SMTP
 * Version:           1.0
-* Author:            Michael Mizner
-* Author URI:        http://miznerism.com
+* Author:            Dj Wysh
+* Author URI:        http://djwysh.rocks
 * Text Domain:       smtp
 * License:           GPL-2.0+
 * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
@@ -23,7 +23,7 @@
 
 add_action( 'phpmailer_init', 'my_phpmailer_knoxweb' );
 function my_phpmailer_knoxweb( $phpmailer ) {
-    $postmark_api = '57b0da66-1087-4822-8f5e-4515b6b03342';
+    $postmark_api = 'dee5e5c2-b1fa-473e-b68f-b7dbc9065bf3';
     $phpmailer->isSMTP();
     $phpmailer->Host = 'smtp.postmarkapp.com';
     $phpmailer->SMTPAuth = true; // Force it to use Username and Password to authenticate
